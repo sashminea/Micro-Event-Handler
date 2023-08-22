af519d6cec99fa1ffc75847078018932b4c360b2
Sashmin <sashminadhikari@gmail.com>
2023-08-22 22:54:40 +0545

    Added Clicker with Reset

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const numParticles = 50;
     const container = document.body;
+    const count = document.getElementById("count");
+    const clicker = document.getElementById("clicker");
+    const reset = document.getElementById("reset");
 
+    function increment() {
 
+        count.textContent = (parseInt(count.textContent) + 1).toString();
+    }  
+    
+    function cleaner() {
+
+        count.textContent = "0";
+    }
+    
+    clicker.addEventListener('click', increment);
+    reset.addEventListener('click', cleaner);
 
     for (let i = 0; i < numParticles; i++) {
         const particle = document.createElement('div');
